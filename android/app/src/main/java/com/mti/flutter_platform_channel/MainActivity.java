b4e1dc597652b1af723f2b99a093aec3462cf724
Tareq Islam <mti.tareq3@gmail.com>
2019-12-22 13:00:27 +0600

    Initial commit

---
+++ android/app/src/main/java/com/mti/flutter_platform_channel/MainActivity.java
@@ -0,0 +1,31 @@
+package com.mti.flutter_platform_channel;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+
+  private static final String CHANNEL= "flutter.native/helper";
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+    new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler((methodCall, result) -> {
+      if(methodCall.method.equals("helloFromNativeCode")){
+        String greeting= helloFromNativeCode();
+        result.success(greeting);
+      }
+    });
+  }
+
+
+  private String helloFromNativeCode(){
+    return "Hello from Native Android Code";
+  }
+
+}
